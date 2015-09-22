4db63b7e224b40170b593ad5622dfe260bc05a70
Aaron Mildenstein <aaron@mildensteins.com>
2015-09-21 18:05:09 -0600

    Put fields into @metadata instead
    This is a breaking change.  This could cause issues for existing
    users.
    
    Make semver-esque major version number bump
    
    Improve internal documentation
    
    Updated documentation
    
    Use add_metadata_from_env
    
    This makes the change a breaking one. `add_metadata_from_env`
    replaces `add_field_from_env`.
    
    Fix doc
    
    Missed the second `add_metadata_from_env` change.
    
    Missed another add_field_from_env ref

--- lib/logstash/filters/environment.rb
+++ lib/logstash/filters/environment.rb
@@ -3,13 +3,43 @@ require "logstash/filters/base"
 require "logstash/namespace"
 
 
-# Set fields from environment variables
+# This filter stores environment variables as subfields in the `@metadata` field.
+# You can then use these values in other parts of the pipeline.
+#
+# Adding environment variables is as easy as:
+#    filter {
+#      environment {
+#        add_metadata_from_env { "field_name" => "ENV_VAR_NAME" }
+#      }
+#    }
+#
+# Accessing stored environment variables is now done through the `@metadata` field:
+#
+#    ["@metadata"]["field_name"]
+#
+# This would reference field `field_name`, which in the above example references
+# the `ENV_VAR_NAME` environment variable.
+#
+# IMPORTANT: Previous versions of this plugin put the environment variables as
+# fields at the root level of the event.  Current versions make use of the
+# `@metadata` field, as outlined.  You have to change `add_field_from_env` in
+# the older versions to `add_metadata_from_env` in the newer version.
 class LogStash::Filters::Environment < LogStash::Filters::Base
   config_name "environment"
 
-  # Specify a hash of fields to the environment variable
-  # A hash of matches of `field => environment` variable
-  config :add_field_from_env, :validate => :hash, :default => {}
+  # Specify a hash of field names and the environment variable name with the
+  # value you want imported into Logstash. For example:
+  #
+  #    add_metadata_from_env { "field_name" => "ENV_VAR_NAME" }
+  #
+  # or
+  #
+  #    add_metadata_from_env {
+  #      "field1" => "ENV1"
+  #      "field2" => "ENV2"
+  #      # "field_n" => "ENV_n"
+  #    }
+  config :add_metadata_from_env, :validate => :hash, :default => {}
 
   public
   def register
@@ -19,8 +49,8 @@ class LogStash::Filters::Environment < LogStash::Filters::Base
   public
   def filter(event)
     return unless filter?(event)
-    @add_field_from_env.each do |field, env|
-      event[field] = ENV[env]
+    @add_metadata_from_env.each do |field, env|
+      event["@metadata"][field] = ENV[env]
     end
     filter_matched(event)
   end # def filter
