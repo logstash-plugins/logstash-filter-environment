9fc015fcec469758f84ba7b25f8a7953e70001c3
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-10-09 11:44:08 -0400

    Filters cannot use `type` to filter events
    Since we are removing the `filter?` method in the base class,
    users will need to to update their configuration and use conditionals
    instead of the `type` option.
    
    Example:
    ```
    filter {
      environment {
        type => "foo"
        add_metadata_from_env => [ "newfield", "MY_ENV_VAR" ]
      }
    }
    ```
    
    Become
    ```
    filter {
      if [type] == "foo" {
        environment {
          type => "foo"
          add_metadata_from_env => [ "newfield", "MY_ENV_VAR" ]
        }
      }
    }
    ```
    
    Fixes #6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
  - Plugins were updated to follow the new shutdown semantic, this mainly allows Logstash to instruct input plugins to terminate gracefully, 
    instead of using Thread.raise on the plugins' threads. Ref: https://github.com/elastic/logstash/pull/3895
  - Dependency on logstash-core update to 2.0
+ - Remove the test for filtering on the `type` since we are removing the `filter?`
 
 1.0.0
   - Use @metadata instead of root-level fields
--- spec/filters/environment_spec.rb
+++ spec/filters/environment_spec.rb
@@ -21,23 +21,4 @@ describe LogStash::Filters::Environment do
       insist { subject["@metadata"]["newfield"] } == "hello world"
     end
   end
-
-  describe "does nothing on non-matching events" do
-    # The logstash config goes here.
-    # At this time, only filters are supported.
-    config <<-CONFIG
-      filter {
-        environment {
-          type => "foo"
-          add_metadata_from_env => [ "newfield", "MY_ENV_VAR" ]
-        }
-      }
-    CONFIG
-
-    ENV["MY_ENV_VAR"] = "hello world"
-
-    sample("type" => "bar", "message" => "fizz") do
-      insist { subject["@metadata"]["newfield"] }.nil?
-    end
-  end
 end
